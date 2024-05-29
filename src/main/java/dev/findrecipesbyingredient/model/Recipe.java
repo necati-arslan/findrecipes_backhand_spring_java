dcd806cc27e9b35ac05b937b9701f8c63a359e4a
necati <necati.arslan2018@gail.com>
2024-05-29 22:31:49 +0200

    model for recipes

--- src/main/java/dev/findrecipesbyingredient/model/Recipe.java
+++ src/main/java/dev/findrecipesbyingredient/model/Recipe.java
@@ -1,5 +1,33 @@
 package dev.findrecipesbyingredient.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Recipe {
 
+    @Id
+    private String Id;
+    private String name;
+
+    public Recipe(String id, String name) {
+        Id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
